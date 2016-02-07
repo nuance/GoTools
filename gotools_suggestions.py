b1ab80c60b5a1cd221e80680e0e5c48b305e1745
Matt Jones <matt@mhjones.org>
2016-02-06 18:20:50 -0800

    Rework autocomplete
    - drop libpath setting for perf
    - inject arg names as snippet

--- gotools_suggestions.py
+++ gotools_suggestions.py
@@ -1,13 +1,45 @@
 import sublime
 import sublime_plugin
 import json
-import os
 
 from .gotools_util import Buffers
 from .gotools_util import GoBuffers
 from .gotools_util import Logger
 from .gotools_util import ToolRunner
 from .gotools_settings import GoToolsSettings
+
+def _lex_func_type(typ):
+  """Convert a function type into list of arguments and return value names"""
+  args = []
+  returns = []
+
+  current = args
+  parens_depth = 0
+
+  val = ''
+  for char in typ[4:].strip():
+    if char == '(':
+      if parens_depth != 0:
+        val += '('
+
+      parens_depth += 1
+    elif char == ')':
+      parens_depth -= 1
+      if parens_depth != 0:
+        val += ')'
+
+      if parens_depth == 0:
+        current.append(val.strip())
+        val = ''
+        current = returns
+    elif char == ',':
+      if parens_depth == 1:
+        current.append(val.strip())
+        val = ''
+    else:
+      val += char
+
+  return args, returns
 
 class GotoolsSuggestions(sublime_plugin.EventListener):
   CLASS_SYMBOLS = {
@@ -18,34 +50,35 @@ class GotoolsSuggestions(sublime_plugin.EventListener):
   }
 
   def on_query_completions(self, view, prefix, locations):
-    if not GoBuffers.is_go_source(view): return
-    if not GoToolsSettings.get().autocomplete: return
+    if not GoBuffers.is_go_source(view) or not GoToolsSettings.get().autocomplete:
+      return
 
-    # set the lib-path for gocode's lookups
-    _, _, rc = ToolRunner.run("gocode", ["set", "lib-path", GoToolsSettings.get().golibpath])
-
-    suggestionsJsonStr, stderr, rc = ToolRunner.run("gocode", ["-f=json", "autocomplete", 
+    suggestions_json_str, stderr, rc = ToolRunner.run("gocode", ["-f=json", "autocomplete", 
       str(Buffers.offset_at_cursor(view)[0])], stdin=Buffers.buffer_text(view))
 
-    # TODO: restore gocode's lib-path
+    suggestions_json = json.loads(suggestions_json_str)
 
-    suggestionsJson = json.loads(suggestionsJsonStr)
-
-    Logger.log("DEBUG: gocode output: " + suggestionsJsonStr)
+    Logger.log("DEBUG: gocode output: " + suggestions_json_str)
 
     if rc != 0:
       Logger.status("no completions found: " + str(e))
       return []
     
-    if len(suggestionsJson) > 0:
-      return ([GotoolsSuggestions.build_suggestion(j) for j in suggestionsJson[1]], sublime.INHIBIT_WORD_COMPLETIONS)
+    if len(suggestions_json) > 0:
+      return ([GotoolsSuggestions.build_suggestion(j) for j in suggestions_json[1]], sublime.INHIBIT_WORD_COMPLETIONS)
     else:
       return []
 
   @staticmethod
   def build_suggestion(json):
+    completion = json["name"]
+    if json["class"] == 'func':
+      args, _ = _lex_func_type(json["type"])
+      snippets = ["${{{0}:{1}}}".format(n + 1, arg.split(' ', 1)[0]) for n, arg in enumerate(args)]
+      completion += '(' + ', '.join(snippets) + ')'
+
     label = '{0: <30.30} {1: <40.40} {2}'.format(
       json["name"],
       json["type"],
       GotoolsSuggestions.CLASS_SYMBOLS.get(json["class"], "?"))
-    return (label, json["name"])
+    return (label, completion)
